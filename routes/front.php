61e576e2ee7ae656c79b8356d97d2fc5997c0f16
HananMohammed <hananmohammed2468@gmail.com>
2020-10-04 13:30:11 +0200

    Adding Route

--- routes/front.php
+++ routes/front.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 /** ADD ALL LOCALIZED ROUTES INSIDE THIS GROUP **/
-Route::get('/front', function()
+Route::get('/', function()
 {
     return view('welcome');
 });
