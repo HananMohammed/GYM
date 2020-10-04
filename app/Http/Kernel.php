47c669b9543db7ba63e0aa6c60fed6a214f936ed
HananMohammed <hananmohammed2468@gmail.com>
2020-10-04 20:18:24 +0200

    Fixes in kernal

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -70,4 +70,5 @@ class Kernel extends HttpKernel
         'localeCookieRedirect'    => \Mcamara\LaravelLocalization\Middleware\LocaleCookieRedirect::class,
         'localeViewPath'          => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationViewPath::class
     ];
+
 }
