ff66c2a6fec765bb9f1cb0092807d707f15aa2da
HananMohammed <hananmohammed2468@gmail.com>
2020-10-14 00:18:37 +0200

    fixes in Sidebar

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -38,21 +38,21 @@
     Essential Scripts
     =====================================-->
 <!-- Main jQuery -->
-<script src="{{asset_public('plugins/jquery/jquery.js')}}"></script>
+<script src="{{asset_public('front/plugins/jquery/jquery.js')}}"></script>
 <!-- Bootstrap 4.3.1 -->
-<script src="{{asset_public('plugins/bootstrap/js/bootstrap.min.js')}}"></script>
+<script src="{{asset_public('front/plugins/bootstrap/js/bootstrap.min.js')}}"></script>
 <!-- Slick Slider -->
-<script src="{{asset_public('plugins/slick-carousel/slick/slick.min.js')}}"></script>
+<script src="{{asset_public('front/plugins/slick-carousel/slick/slick.min.js')}}"></script>
 <!--  Magnific Popup-->
-<script src="{{asset_public('plugins/magnific-popup/dist/jquery.magnific-popup.min.js')}}"></script>
+<script src="{{asset_public('front/plugins/magnific-popup/dist/jquery.magnific-popup.min.js')}}"></script>
 <!-- Form Validator -->
 <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery.form/3.32/jquery.form.js"></script>
 <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.11.1/jquery.validate.min.js"></script>
 <!-- Google Map -->
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBu5nZKbeK-WHQ70oqOWo-_4VmwOwKP9YQ"></script>
-<script src="{{asset_public('plugins/google-map/gmap.js')}}"></script>
+<script src="{{asset_public('front/plugins/google-map/gmap.js')}}"></script>
 
-<script src="{{asset_public('js/script.js')}}"></script>
+<script src="{{asset_public('front/js/script.js')}}"></script>
 @yield('scripts')
 </body>
 
--- routes/front.php
+++ routes/front.php
@@ -17,6 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function()
 {
     return view('front.index');
-});
+})->name('homepage');
 
 
